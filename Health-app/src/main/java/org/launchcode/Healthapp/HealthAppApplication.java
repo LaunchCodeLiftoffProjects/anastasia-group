e8e46e5ed5744e27dacd1f202b5f4ddedf4668ec
Kelly Reed <kellyreed@kellys-mbp.lan>
2022-01-06 19:41:14 -0600

    started structuring project

---
+++ Health-app/src/main/java/org/launchcode/Healthapp/HealthAppApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Healthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthAppApplication.class, args);
+	}
+
+}
---
+++ Health-app/src/test/java/org/launchcode/Healthapp/HealthAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Healthapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
