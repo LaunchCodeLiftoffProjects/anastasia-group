67149e32a16eb4edf31bd1e9112e9490c85ff855
kaixin2017 <luhuang021@gmail.com>
2022-02-11 16:06:14 -0600

    workingcorrectly

--- Health-Application/src/main/java/org/launchcode/Health/Application/Controllers/AuthenticationController.java
+++ Health-Application/src/main/java/org/launchcode/Health/Application/Controllers/AuthenticationController.java
@@ -118,7 +118,7 @@ public class AuthenticationController {
 
         setUserInSession(request.getSession(), theUser);
 
-        return "/personalInformation";
+        return "redirect:/home";
     }
 
     @GetMapping("/logout")
